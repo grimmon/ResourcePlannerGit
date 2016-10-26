ddf1f3d8a9511522057ccc60ce4618b763683e42
gmorini <gmorini@bluemetal.com>
2016-10-26 10:42:43 -0400

    refactor.

---
+++ ResourcePlanner.Services/Scripts/ResourceDetailGridJs.js
@@ -0,0 +1,110 @@
+﻿var resourceDetailGrid = {
+    name: '#myGrid2',
+    options: {
+        debug: true,
+        enableServerSideSorting: true,
+        enableServerSideFilter: true,
+        enableColResize: true,
+        rowSelection: 'single',
+        rowDeselection: true,
+        columnDefs: startingResourceDetailColumnDefs,
+        rowModelType: 'virtual',
+        paginationPageSize: 30,
+        paginationOverflowSize: 2,
+        maxConcurrentDatasourceRequests: 2,
+        paginationInitialRowCount: 1,
+        maxPagesInCache: 6,
+
+        getRowNodeId: function (item) {
+            return item.Id;
+        }
+    }
+};
+
+var startingResourceDetailColumnDefs = [
+    { headerName: "Project Name", field: "ProjectName", width: 150, suppressMenu: true, pinned: true, cellRenderer: loadingCellRenderer },
+];
+
+function initializeResourceDetailGrid() {
+    var gridDiv = document.querySelector(resourceDetailGrid.name);
+    new agGrid.Grid(gridDiv, resourceDetailGrid.options);
+}
+
+function refreshResourceDetailGrid(event) {
+    if (event.node.isSelected()) {
+        resourceDetailGrid.options.api.showLoadingOverlay();
+
+        selectedResource.Id = event.node.data.Id;
+
+        var dataSource = {
+            rowCount: null, // behave as infinite scroll
+            getRows: function getResourceDetailData(params) {
+                var query = buildResourceDetailQuery(params);
+                callResourceServerAuth(params, query, onCallResourceDetailSuccess, onCallResourceDetailFailure);
+            }
+        };
+
+        $("#selectedUser").text("loading...");
+        resourceDetailGrid.options.api.setDatasource(dataSource);
+        updateSelectedUser(event.node.data);
+    }
+}
+
+function updateSelectedUser(data) {
+    $("#selectedUser").text("first Name: " + data.FirstName + ", Last Name: " + data.LastName);
+}
+
+function buildResourceDetailQuery(params) {
+    var filters = '?';
+
+    if (params.Id !== null) {
+        filters += "ResourceId=" + selectedResource.Id;
+    }
+
+    var query = 'api/resourcedetail' + filters;
+
+    return query;
+}
+
+function onCallResourceDetailSuccess(params, query, httpResponse) {
+    updateResourceDetailGrid(params, httpResponse, httpResponse.Projects, httpResponse.TimePeriods, resourceDetailGrid.options);
+}
+
+function updateResourceDetailGrid(params, data, rowData, columnData, options) {
+    var columns = createResourceColumns(startingResourceDetailColumnDefs, data);
+    var rows = createRows(rowData, columnData, createProjectRow);
+
+    var columnsChanged = checkForColumnChanges(currentColumns, columns);
+
+    if (columnsChanged) {
+        options.api.setColumnDefs(columns);
+        currentColumns = columns;
+    }
+
+    params.successCallback(rows, data.TotalRowCount);
+    resourceDetailGrid.options.api.hideOverlay();
+}
+
+function onCallResourceDetailFailure(httpRequest) {
+    resourceDetailGrid.options.api.hideOverlay();
+    $("#selectedUser").text("Error: " + httpRequest.statusText + ", " + httpRequest.responseText);
+}
+
+function createProjectRow(row, project, timePeriods) {
+    addProjectDetails(row, project            );
+    addTimePeriods   (row, timePeriods        );
+    addAssignments   (row, project.Assignments);
+}
+
+function addProjectDetails(row, project) {
+    row.ProjectName               = project.ProjectName;
+    row.FirstName                 = project.FirstName;
+    row.LastName                  = project.LastName;
+    row.WBSElement                = project.WBSElement; 
+    row.Customer                  = project.Customer; 
+    row.Description               = project.Description;
+    row.OpportunityOwnerFirstName = project.OpportunityOwnerFirstName;
+    row.OpportunityOwnerLastName  = project.OpportunityOwnerLastName 
+    row.ProjectManagerFirstName   = project.ProjectManagerFirstName;
+    row.ProjectManagerLastName    = project.ProjectManagerLastName;
+}
---
+++ ResourcePlanner.Services/Scripts/ResourceGridJs.js
@@ -0,0 +1,128 @@
+﻿var resourceGrid = {
+    name: '#myGrid',
+    options: {
+        debug: true,
+        enableServerSideSorting: true,
+        enableServerSideFilter: true,
+        enableColResize: true,
+        rowSelection: 'single',
+        rowDeselection: true,
+        columnDefs: startingResourceColumnDefs,
+        rowModelType: 'virtual',
+        paginationPageSize: 30,
+        paginationOverflowSize: 2,
+        maxConcurrentDatasourceRequests: 2,
+        paginationInitialRowCount: 1,
+        maxPagesInCache: 6,
+        onRowSelected: rowSelectedFunc,
+        getRowNodeId: function (item) {
+            return item.Id;
+        }
+    }
+};
+
+var startingResourceColumnDefs = [
+    { headerName: "First Name", field: "FirstName", width: 150, suppressMenu: true, pinned: 'left', cellRenderer: loadingCellRenderer },
+    { headerName: "Last Name" , field: "LastName" , width: 150, suppressMenu: true, pinned: 'left'},
+    { headerName: "Position"  , field: "Position" , width: 150, suppressMenu: true },
+    { headerName: "City"      , field: "City"     , width: 150, suppressMenu: true },
+];
+
+function initializeResourceGrid() {
+    var gridDiv = document.querySelector(resourceGrid.name);
+    new agGrid.Grid(gridDiv, resourceGrid.options);
+
+    refreshResourceGrid();
+}
+
+function refreshResourceGrid() {
+    resourceGrid.options.api.showLoadingOverlay();
+
+    var dataSource = {
+        rowCount: null, // behave as infinite scroll
+        getRows: function getResourceData(params) {
+            var query = buildResourceQuery(params);
+            callResourceServerAuth(params, query, onCallResourceSuccess, onCallResourceFailure);
+        }
+    };
+
+    resourceGrid.options.api.setDatasource(dataSource);
+    resourceGrid.options.api.refreshHeader();
+}
+
+function buildResourceQuery(params) {
+    var pageSize = (params.endRow - params.startRow);
+    var pageNum = params.startRow / pageSize;
+
+    console.log('asking for ' + params.startRow + ' to ' + params.endRow);
+
+    var filters = '?';
+
+    var pageSizeParam = 'pageSize=' + pageSize;
+    var pageNumParam  = '&pageNum=' + pageNum;
+
+    filters += pageSizeParam + pageNumParam;
+
+    var city     = document.getElementById('citiesDropdown'   ).value;
+    var orgUnit  = document.getElementById('orgUnitsDropdown' ).value;
+    var region   = document.getElementById('regionsDropdown'  ).value;
+    var market   = document.getElementById('marketsDropdown'  ).value;
+    var practice = document.getElementById('practicesDropdown').value;
+    
+    if (city     != -1 && city     != '') { filters += "&city="     + city;     }
+    if (orgUnit  != -1 && orgUnit  != '') { filters += "&orgUnit="  + orgUnit;  }
+    if (region   != -1 && region   != '') { filters += "&region="   + region;   }
+    if (market   != -1 && market   != '') { filters += "&market="   + market;   }
+    if (practice != -1 && practice != '') { filters += "&practice=" + practice; }
+
+    if (params.sortModel.length > 0) {
+        filters += '&sortOrder='     + params.sortModel[0].colId;
+        filters += "&sortDirection=" + params.sortModel[0].sort;
+    }
+
+    var query = 'api/resource' + filters;
+
+    return query;
+}
+
+function onCallResourceSuccess(params, query, httpResponse) {
+    var columns = createResourceColumns(startingResourceColumnDefs, httpResponse);
+    var rows = createRows(httpResponse.Resources, httpResponse.TimePeriods, createResourceRow);
+
+    var columnsChanged = checkForColumnChanges(currentColumns, columns);
+
+    if (columnsChanged) {
+        resourceGrid.options.api.setColumnDefs(columns);
+
+        var detailColumns = createResourceColumns(startingResourceDetailColumnDefs, httpResponse);
+        resourceDetailGrid.options.api.setColumnDefs(columns);
+
+        currentColumns = columns;
+    }
+
+    params.successCallback(rows, httpResponse.TotalRowCount);
+    resourceGrid.options.api.hideOverlay();
+}
+
+function onCallResourceFailure(httpRequest) {
+    resourceGrid.options.api.hideOverlay();
+    $("#selectedUser").text("Error: " + httpRequest.statusText + ", " + httpRequest.responseText);
+}
+
+function createResourceRow(row, resource, timePeriods) {
+    addResourceDetails(row, resource            );
+    addTimePeriods    (row, timePeriods         );
+    addAssignments    (row, resource.Assignments);
+}
+
+function addResourceDetails(row, resource) {
+    row.FirstName = resource.FirstName;
+    row.LastName  = resource.LastName;
+    row.City      = resource.City;
+    row.Position  = resource.Position;
+    row.Id        = resource.ResourceId;
+}
+
+function rowSelectedFunc(event) {
+    refreshResourceDetailGrid(event);
+}
