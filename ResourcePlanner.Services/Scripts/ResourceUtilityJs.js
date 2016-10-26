814a2d6c7fb6ae14ae15076bbeb168879641cee7
gmorini <gmorini@bluemetal.com>
2016-10-26 11:51:14 -0400

    removed virtualPagingServerSide file.

--- ResourcePlanner.Services/Scripts/ResourceUtilityJs.js
+++ ResourcePlanner.Services/Scripts/ResourceUtilityJs.js
@@ -3,6 +3,42 @@ var columnHeaders = {};
 var selectedResource = {};
 var currentColumns = [];
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    var authContext = new AuthenticationContext(config);
+
+    authContext.acquireToken(authContext.config.clientId, function (error, token) {
+        if (error || !token) {
+            alert('ADAL Error: ' + error);
+            return;
+        }
+        getDropDownData(token);
+    });
+
+    var filterButton = document.getElementById("filterButton");
+
+    filterButton.onclick = function () {
+        count++;
+        refreshResourceGrid();
+    }
+
+    var modal = document.getElementById("errorModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    initializeResourceGrid();
+    initializeResourceDetailGrid();
+});
+
 function addTimePeriods(row, timePeriods) {
     for (var i = 0; i < timePeriods.length; i++) {
         var timePeriod = timePeriods[i];
