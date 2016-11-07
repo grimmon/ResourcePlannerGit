b310cfc33fd0169aa4d26fff1138f32d8545a29d
grimmon@bluemetal.com <grimmon@bluemetal.com>
2016-11-07 11:45:11 -0500

    Adding Project search to services

---
+++ ResourcePlanner.Services/DataAccess/ProjectDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using ResourcePlanner.Core.Utilities;
+using ResourcePlanner.Services.Mapper;
+using ResourcePlanner.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ResourcePlanner.Services.DataAccess
+{
+    public class ProjectDataAccess
+    {
+        private readonly string _connectionString;
+        private readonly int _timeout;
+
+        public ProjectDataAccess(string connectionString, int timeout)
+        {
+            _connectionString = connectionString;
+            _timeout = timeout;
+        }
+
+
+        public List<IdNameGeneric> GetProjects(string searchTerm)
+        {
+
+            var returnValue = AdoUtility.ExecuteQuery(reader => EntityMapper.MapToIdNameGeneric(reader, "ProjectId", ""),
+                _connectionString,
+                @"rpdb.ProjectSelect",
+                CommandType.StoredProcedure,
+                _timeout,
+                new SqlParameter[] { searchTerm != "" 
+                                        ? AdoUtility.CreateSqlParameter("SearchTerm", 50, SqlDbType.VarChar, searchTerm) 
+                                        : null });
+            return returnValue;
+        }
+    }
+}
