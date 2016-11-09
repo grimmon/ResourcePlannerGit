d96c000e2217c8739ccbc6e4c20b2ebe5a451f36
grimmon@bluemetal.com <grimmon@bluemetal.com>
2016-11-09 11:13:30 -0500

    Assignment grid changes

---
+++ ResourcePlanner.Services/DropdownService.ts
@@ -0,0 +1,17 @@
+﻿import { IDropdownValue } from './IDropdownValue';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DropdownService {
+    private _resourceUrl = 'api/resourcedetail';
+
+    constructor(private _http: Http) { }
+
+    getProducts(): Observable<IDropdownValue[]> {
+        return this._http.get(this._resourceUrl)
+            .map((response: Response) => <IDropdownValue[]>response.json());
+    }
+}
