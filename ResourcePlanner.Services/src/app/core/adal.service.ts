2bf9c1692cb64107d3cf4b6027a98d2566cfdd5c
grimmon@bluemetal.com <grimmon@bluemetal.com>
2017-01-17 13:01:28 -0500

    put adal into scripts

--- ResourcePlanner.Services/src/app/core/adal.service.ts
+++ ResourcePlanner.Services/src/app/core/adal.service.ts
@@ -1,32 +1,36 @@
+
+
 import { Injectable } from '@angular/core';
+
+declare var adal: any;
 
 @Injectable()
 export class AdalService {
 
     private authContext: any;
 
-    init = (config: adal.Config) => {
-        var authContext: any = new AuthenticationContext(config);
+    init = (config: any) => {
+        //var authContext: any = new AuthenticationContext(config);
 
-        // Check For & Handle Redirect From AAD After Login
-        var isCallback = authContext.isCallback(window.location.hash);
-        authContext.handleWindowCallback();
+        //// Check For & Handle Redirect From AAD After Login
+        //var isCallback = authContext.isCallback(window.location.hash);
+        //authContext.handleWindowCallback();
 
-        if (isCallback && !authContext.getLoginError()) {
-            //window.location = authContext._getItem(authContext.CONSTANTS.STORAGE.LOGIN_REQUEST);
-        }
+        //if (isCallback && !authContext.getLoginError()) {
+        //    //window.location = authContext._getItem(authContext.CONSTANTS.STORAGE.LOGIN_REQUEST);
+        //}
 
-        // Check Login Status, Update UI;
-        if (!authContext.getCachedUser()) {
-            authContext.config.redirectUri = window.location.href;
-            authContext.login();
-            return;
-        }
-        authContext.acquireToken(authContext.config.clientId, function (error: any, token: any) {
-            if (token) {
-                localStorage.setItem('id_token', token);
-            }
-        });
+        //// Check Login Status, Update UI;
+        //if (!authContext.getCachedUser()) {
+        //    authContext.config.redirectUri = window.location.href;
+        //    authContext.login();
+        //    return;
+        //}
+        //authContext.acquireToken(authContext.config.clientId, function (error: any, token: any) {
+        //    if (token) {
+        //        localStorage.setItem('id_token', token);
+        //    }
+        //});
     }
 
     logout = () => {
